7cd6762d6d6e86953696d77c92ddda335e9b21e5
ishanrana363 <ishanrana363@gmail.com>
2025-05-09 10:09:50 -0800

    tume for artist page completed

--- src/pages/home-page/Coverd.tsx
+++ src/pages/home-page/Coverd.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Coverd = () => {
-    
+
     return (
-        <div className=' max-w-[1539px] mx-auto px-4 mt-10 lg:mt-[91px] ' >
+        <div className=' max-w-[1539px] mx-auto px-4 mt-10 lg:mt-[91px]  ' >
             <div className=' flex lg:flex-row flex-col items-start justify-between relative gap-5 ' >
                 {/* left side  */}
                 <div>
@@ -51,9 +52,41 @@ const Coverd = () => {
                 </div>
                 {/* right side  */}
                 <div>
-                    <Image src={"/images/home-page/coveredImg.png"} className=' object-cover rounded-lg ' width={652} height={654} alt='....' />
+                    <Image src={"/images/home-page/coveredImg.png"} className=' object-cover rounded-lg block mx-auto ' width={652} height={654} alt='....' />
                 </div>
             </div>
+
+
+            <div
+                className="bg-[url('/images/home-page/tunemImg.png')] bg-no-repeat bg-cover bg-center py-6 lg:pt-11 lg:pb-16 rounded-lg mt-16 lg:mt-[107px] relative overflow-hidden"
+            >
+                {/* Gradient Overlay */}
+                <div className="absolute inset-0 bg-gradient-to-b from-black to-black/30 z-0"></div>
+
+                <div className="relative z-10">
+                    <h1 className="text-center text-[#E7F056] font-bold lg:text-lg">
+                        TUNEM FOR ARTISTS
+                    </h1>
+                    <div className="mx-auto mt-5 lg:mt-16">
+                        <h1 className="text-center lg:text-7xl text-3xl text-white font-thin">
+                            GROW YOUR <br />
+                            REACH & AUDIENCE
+                        </h1>
+                    </div>
+                    <div className="max-w-[482px] mx-auto mt-3 lg:mt-9">
+                        <p className="text-center text-white leading-6 lg:text-xl font-thin ">
+                            Whether you want to promote your own song, increase revenue or reach a
+                            wider audience, artists always benefit from our artists-first approach.
+                        </p>
+                    </div>
+                    <div>
+                        <button className="text-[#E7F056] lg:text-lg w-[194px] py-2 border border-white rounded-2xl block mx-auto mt-4 lg:mt-13">
+                            <Link href={""}>GET STARTED</Link>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
